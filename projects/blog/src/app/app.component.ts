d2a63c9fd465d4e705c89f658fc2b65d67af70df
Rodrigo <rodrigodsq1@gmail.com>
2023-01-22 20:35:08 -0300

    feat: dispatch login user - loading in app

--- projects/blog/src/app/app.component.ts
+++ projects/blog/src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ProfileApi } from './core/api/profile.api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'blog';
+export class AppComponent
+{
+    public loading$: Observable<boolean> = this.profileApi.profileLoading$;
+
+    constructor(
+        private readonly profileApi: ProfileApi
+    )
+    { }
+
+    public ngOnInit(): void
+    {
+        this.profileApi.loginUser(1);
+    }
 }
