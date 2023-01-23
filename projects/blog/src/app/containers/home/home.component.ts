db8f07c27ad52d2d12a44dcd61a866fb8526798c
Rodrigo <rodrigodsq1@gmail.com>
2023-01-23 02:27:58 -0300

    feat: show data of author postHome

--- projects/blog/src/app/containers/home/home.component.ts
+++ projects/blog/src/app/containers/home/home.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, HostListener, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { PostHomeApi } from '../../core/api/post-home.api';
+import { PostHome } from '../../core/models/post-home';
 
 @Component({
     selector: 'app-home',
@@ -11,6 +12,7 @@ import { PostHomeApi } from '../../core/api/post-home.api';
 export class HomeComponent implements OnInit
 {
     public loading$: Observable<boolean> = this.postHomeApi.postHomeLoading$;
+    public home$: Observable<PostHome> = this.postHomeApi.postHome$;
 
     public headerHeight: string = '';
 
--- projects/blog/src/app/core/models/post-home.ts
+++ projects/blog/src/app/core/models/post-home.ts
@@ -7,6 +7,7 @@ export interface Author
 {
     id: number;
     username: string;
+    avatar: string;
 }
 
 export interface Comment
@@ -45,18 +46,20 @@ export const mockPostHome: RawPost = {
     timestamp: '2019-02-20T13:22Z',
     author: {
         id: 1,
-        username: 'João Figueiredo'
+        username: 'João Figueiredo',
+        avatar: "/assets/avatars/avatar1.jpg",
     },
     title: 'Estratégias em um novo paradigma globalizado',
     subtitle: 'Sobre o cuidado em identificar pontos críticos na complexidade',
-    content: '<p>Caros amigos, a mobilidade dos capitais internacionais desafia a capacidade de equalização das diversas correntes de pensamento. Nunca é demais lembrar o peso e o significado destes problemas, uma vez que a necessidade de renovação processual ainda não demonstrou convincentemente que vai participar na mudança das diretrizes de desenvolvimento para o futuro. O cuidado em identificar pontos críticos na complexidade dos estudos efetuados é uma das consequências das direções preferenciais no sentido do progresso. Do mesmo modo, a adoção de políticas descentralizadoras não pode mais se dissociar dos modos de operação convencionais.</p>',
+    content: '<p>Caros amigos, a mobilidade dos capitais internacionais desafia a capacidade de equalização das diversas correntes de pensamento. Nunca é demais lembrar o peso e o significado destes problemas, uma vez que a necessidade de renovação processual ainda não demonstrou convincentemente que vai participar na mudança das diretrizes de desenvolvimento para o futuro. O cuidado em identificar pontos críticos na complexidade dos estudos efetuados é uma das consequências das direções preferenciais no sentido do progresso. Do mesmo modo, a adoção de políticas descentralizadoras não pode mais se dissociar dos modos de operação convencionais.</p> <br> <p> Acima de tudo, é fundamental ressaltar que o novo modelo estrutural aqui preconizado cumpre um papel essencial na formulação das novas proposições. A nível organizacional, o desenvolvimento contínuo de distintas formas de atuação oferece uma interessante oportunidade para verificação dos conhecimentos estratégicos para atingir a excelência. Por outro lado, a constante divulgação das informações exige a precisão e a definição do sistema de formação de quadros que corresponde às necessidades. É claro que a contínua expansão de nossa atividade estimula a padronização das condições inegavelmente apropriadas. </p>',
     comments: [
         {
             id: 1,
             respondsTo: null,
             author: {
                 id: 2,
-                username: 'Joana Vasconcellos'
+                username: 'Joana Vasconcellos',
+                avatar: "/assets/avatars/avatar2.jpg",
             },
             timestamp: '2019-02-20:30Z',
             content: 'O empenho em analisar a consolidação das estruturas oferece uma interessante oportunidade para verificação do retorno esperado a longo prazo. Por outro lado, o julgamento imparcial das eventualidades facilita a criação dos modos de operação convencionais. O que temos que ter sempre em mente é que a revolução dos costumes estimula a padronização das novas proposições.'
@@ -66,7 +69,8 @@ export const mockPostHome: RawPost = {
             respondsTo: null,
             author: {
                 id: 3,
-                username: 'Arthur Silveira'
+                username: 'Arthur Silveira',
+                avatar: "/assets/avatars/avatar3.jpg",
             },
             timestamp: '2019-02-17T11:23Z',
             content: 'É importante questionar o quanto o consenso sobre a necessidade de qualificação possibilita uma melhor visão global dos índices pretendidos.'
@@ -78,7 +82,8 @@ export const mockPostHome: RawPost = {
             },
             author: {
                 id: 4,
-                username: 'Clara Passos'
+                username: 'Clara Passos',
+                avatar: "/assets/avatars/avatar4.jpg",
             },
             timestamp: '2019-02-23T07:48Z',
             content: 'Ainda assim, existem dúvidas a respeito de como a execução dos pontos do programa representa uma abertura para a melhoria da gestão inovadora da qual fazemos parte.'
@@ -90,7 +95,8 @@ export const mockPostHome: RawPost = {
             },
             author: {
                 id: 5,
-                username: 'Mauro Andrade'
+                username: 'Mauro Andrade',
+                avatar: "/assets/avatars/avatar5.jpg",
             },
             timestamp: '2019-02-28T07:08Z',
             content: 'Concordo plenamente, Clara!'
@@ -102,7 +108,8 @@ export const mockPostHome: RawPost = {
             },
             author: {
                 id: 6,
-                username: 'Rafaela Moreira'
+                username: 'Rafaela Moreira',
+                avatar: "/assets/avatars/avatar6.jpg",
             },
             timestamp: '2019-02-28T08:21Z',
             content: 'Discordo veementemente, Clara!'
