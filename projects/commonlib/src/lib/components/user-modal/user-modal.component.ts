b2b0d1f58ac22df5120bdea65da13192bc8b9db5
Rodrigo <rodrigodsq1@gmail.com>
2023-01-24 18:02:35 -0300

    feat: ui modal user

--- projects/commonlib/src/lib/components/user-modal/user-modal.component.ts
+++ projects/commonlib/src/lib/components/user-modal/user-modal.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-import { UserDetails } from './user.model';
+import { Posts, UserDetails } from './user.model';
 
 @Component({
     selector: 'wui-user-modal',
@@ -11,6 +11,8 @@ export class UserModalComponent
 {
     public show: boolean = false;
     public user: UserDetails;
+    public sliderItems: Array<Array<Posts>> = [[]];
+    public sliderIndex: number = 0;
 
     @Input()
     public set userDetails(value: UserDetails)
@@ -22,10 +24,46 @@ export class UserModalComponent
 
         this.user = value;
         this.show = true;
+        this.initSlider(value.posts);
+    }
+
+    public initSlider(value: Array<Posts>): void
+    {
+        let count: number = 0;
+
+        value.forEach((post) => {
+            if(this.sliderItems[count].length < 3)
+            {
+                this.sliderItems[count].push(post);
+                return
+            }
+            count = count +1;
+            this.sliderItems.push([])
+            this.sliderItems[count].push(post);
+        });
+    }
+
+    public changeSlider(isMore: boolean): void
+    {
+        if( isMore && this.sliderIndex < (this.sliderItems.length - 1))
+        {
+            this.sliderIndex = this.sliderIndex + 1;
+        }
+
+        if( !isMore && this.sliderIndex > 0)
+        {
+            this.sliderIndex = this.sliderIndex - 1;
+        }
     }
 
 
     public toggle () {
         this.show = !this.show;
+
+        if(!this.show)
+        {
+            this.sliderIndex = 0;
+            this.sliderItems = [[]];
+        }
     }
 }
